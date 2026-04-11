cd84907a8e616e1c4336f38b09b7fec36f105de6
sarkartukai25 <sarkartukai25@gmail.com>
2026-04-11 18:55:01 +0000

    feat: initial commit

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  allowedDevOrigins: ['3000-firebase-image-code-1775930655047.cluster-fkltigo73ncaixtmokrzxhwsfc.cloudworkstations.dev'],
+};
+
+export default nextConfig;
